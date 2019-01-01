09d864a33d67412f07f008b78fbb601f445c0cf6
lihaocheng <lihaocheng00@126.com>
2019-01-01 15:34:47 +0800

    init

---
+++ src/main/java/com/principle/openclose/ICourse.java
@@ -0,0 +1,13 @@
+package com.principle.openclose;
+
+/**
+ * @author lihaocheng
+ * @create 2019-01-01 3:26 PM
+ **/
+
+public interface ICourse {
+    Integer getId();
+    String getName();
+    Double getPrice();
+
+}
